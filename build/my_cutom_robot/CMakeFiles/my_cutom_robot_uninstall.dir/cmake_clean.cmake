6671291ad481695971231920fa28adfed35a2e3d
yiming <pym66@outlook.com>
2023-08-07 19:55:50 +0800

    Add urdf files

---
+++ build/my_cutom_robot/CMakeFiles/my_cutom_robot_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_cutom_robot_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/my_cutom_robot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/my_cutom_robot/README.md
@@ -0,0 +1,5 @@
+## Tips:
+
+ros2 run joint_state_publisher_gui joint_state_publisher_gui 
+
+let your joint visible.
