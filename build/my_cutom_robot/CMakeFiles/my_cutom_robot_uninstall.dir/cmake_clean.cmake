4c593ba19cf937243e545275df965c829ab40caf
YiMIng <105438207+pym96@users.noreply.github.com>
2025-04-21 15:04:19 +0800

    Delete build directory

--- build/my_cutom_robot/CMakeFiles/my_cutom_robot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/my_cutom_robot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/my_cutom_robot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
